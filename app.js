3daa2810d4ed38e1cb272372aa398c3aefd5eebd
YinWei <cba5530@gmail.com>
2021-01-10 23:10:16 +0800

    feat: filter same inputUrl

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -14,6 +15,7 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const generateLastKeyword = require('../../keyword_generator')
+const Url = require('../../models/url')
 
 
 router.get('/', (req, res) => {
@@ -10,10 +11,34 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  let lastKeyword = generateLastKeyword(5)
+  let pairUrl = generateLastKeyword(5)
+  const inputUrl = req.body.inputUrl
 
-  res.render('index', { lastKeyword })
+  Url.exists({ inputUrl: `${inputUrl}` }, function (err, doc) {
+    if (err) {
+      console.log(err)
+    } else if (doc) {
+      Url.findOne({ inputUrl: `${inputUrl}` }).lean()
+        .then((item) => {
+          pairUrl = item.pairUrl
+          res.render('index', { pairUrl })
+        })
+        .catch(error => console.log(error))
+    } else {
+      return Url.create({ inputUrl, pairUrl })
+        .then(() => res.render('index', { pairUrl }))
+        .catch(error => console.log(error))
+    }
+  })
 
 })
+
+router.get('/:pairUrl', (req, res) => {
+  const pairUrl = req.params.pairUrl
+  Url.findOne({ pairUrl: `${pairUrl}` }).lean()
+    .then(item => res.redirect(`${item.inputUrl}`))
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
