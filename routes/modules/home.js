8a17f2947d01ddefcd44df8da9a656843103cab3
YinWei <cba5530@gmail.com>
2021-01-11 22:18:43 +0800

    feat: add remarks

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,14 +11,16 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  const website = req.headers.host
+  const website = req.headers.referer
   let pairUrl = generateLastKeyword(5)
   const inputUrl = req.body.inputUrl
 
+  //確認轉換之網址是否重複
   Url.exists({ inputUrl: `${inputUrl}` }, function (err, doc) {
     if (err) {
       console.log(err)
     } else if (doc) {
+      //若重複則撈出一樣的短網址
       Url.findOne({ inputUrl: `${inputUrl}` }).lean()
         .then((item) => {
           pairUrl = item.pairUrl
@@ -26,6 +28,7 @@ router.post('/', (req, res) => {
         })
         .catch(error => console.log(error))
     } else {
+      //若不重複則給出短網址
       return Url.create({ inputUrl, pairUrl })
         .then(() => res.render('index', { pairUrl, website }))
         .catch(error => console.log(error))
@@ -34,6 +37,7 @@ router.post('/', (req, res) => {
 
 })
 
+//若輸入短網址，則導向目標網址
 router.get('/:pairUrl', (req, res) => {
   const pairUrl = req.params.pairUrl
   Url.findOne({ pairUrl: `${pairUrl}` }).lean()
